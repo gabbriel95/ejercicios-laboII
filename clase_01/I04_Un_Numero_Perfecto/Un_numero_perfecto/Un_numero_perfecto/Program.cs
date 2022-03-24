7bf49ac55a2370fadddb34b6abf31afa9702fc62
gabbriel95 <juanmendez0013@gmail.com>
2022-03-24 17:16:51 -0600

    4to terminado

---
+++ clase_01/I04_Un_Numero_Perfecto/Un_numero_perfecto/Un_numero_perfecto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RangoPerfectos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador; int num; int j;
+            int contadorPerfectos = 0;
+
+            num = Int16.MaxValue;
+
+            if (contadorPerfectos <= 4) 
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    acumulador = 0;
+
+                    for (j = 1; j <= (i / 2); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            acumulador += j;
+                        }
+                    }
+                    if (acumulador == i)
+                    {
+                        contadorPerfectos++;
+                        Console.WriteLine($"Numero perfecto #{contadorPerfectos} es el numero {i} es perfecto");
+                    }
+                }
+            }
+        }
+    }
+}
