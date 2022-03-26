bfd74c6ce6b3d6295435ecde069f19cd6b3bcd7d
gabbriel95 <juanmendez0013@gmail.com>
2022-03-26 17:31:13 -0600

    IO2 clase 2 terminado

---
+++ Clase_02/I02_Desea_Continuar/I02_Desea_Continuar/I02_Desea_Continuar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LogicaNegocio;
+
+namespace I02_Desea_Continuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            string numeroIngresadoString;
+            string respuesta;
+            int acumulador = 0;
+
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese un numero");
+                    numeroIngresadoString = Console.ReadLine();
+
+                } while (!int.TryParse(numeroIngresadoString, out numeroIngresado));
+
+                acumulador = Suma(acumulador, numeroIngresado);
+
+                Console.WriteLine("Desea sumar otro numero?");
+                respuesta = Console.ReadLine();
+
+            } while (Validador.ValidarRespuesta(respuesta));
+
+
+            Console.WriteLine($"La suma de todos los numeros ingresados es {acumulador}");
+
+
+        }
+
+        static int Suma(int numeroUno, int numeroDos)
+        {
+            int resultado = numeroUno + numeroDos;
+
+            return resultado;
+        }
+    }
+}
---
+++ Clase_02/I02_Desea_Continuar/I02_Desea_Continuar/LogicaNegocio/Validador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Validador
+    {
+        public static bool ValidarRespuesta(string respuesta) {
+
+            bool validacion = false;
+            if (respuesta == "S" || respuesta == "s") {
+            
+                validacion = true; 
+            }
+
+            return validacion;
+        }
+    }
+}
