f74dfc73f30f94f73b5c015cc62c0891615340d8
gabbriel95 <juanmendez0013@gmail.com>
2022-03-27 00:20:09 -0300

    03 terminado

---
+++ Clase_02/I03_Conversor_Binario/Vista/LogicaNegocio/Conversor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Conversor
+    {
+        public static string ConvertirDecimalABinario(int numeroEntero) {
+            
+            string numeroBinario = "";
+            int divisor = 2;
+            int resto;
+
+            for (int i = 0; numeroEntero >= divisor; i++)
+            {
+                resto = numeroEntero % 2;
+                numeroEntero = numeroEntero / divisor;
+
+                numeroBinario += resto;
+
+                if (numeroEntero < divisor)
+                {
+                    numeroBinario += numeroEntero;
+                    numeroBinario = Reverse(numeroBinario);
+                  
+                }
+            }
+
+
+            return numeroBinario;
+        }
+
+        private static string Reverse(string text) {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static int ConvertirBinarioADecimal(int numeroEntero) {
+            int numeroDecimal = 0;
+            string numeroString = numeroEntero.ToString();
+            int numeroIndividual;
+            double multiplicador = 0;
+            double acumulador = 0;
+
+            for (int i = 0; i < numeroString.Length; i++)
+            {
+                int.TryParse(numeroString.Substring(i, 1), out numeroIndividual);
+                multiplicador = Math.Pow(2, (numeroString.Length-1)-i);
+                acumulador += multiplicador * numeroIndividual;
+            }
+                Console.WriteLine($"{acumulador}");
+
+
+            return numeroDecimal;
+        }
+    }
+}
---
+++ Clase_02/I03_Conversor_Binario/Vista/Vista/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LogicaNegocio;
+
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // string numero = Conversor.ConvertirDecimalABinario(100);
+            //Console.WriteLine($"{numero}");
+
+            int numero2 = Conversor.ConvertirBinarioADecimal(1100100);
+            //Console.WriteLine($"{numero2}");
+        }
+    }
+}
