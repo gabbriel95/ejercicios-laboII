87cea4f22ba2c472ae337f3de50864afcd03b996
gabbriel95 <juanmendez0013@gmail.com>
2022-03-27 21:25:46 -0300

    06 terminado

---
+++ Clase_02/I06_Calculadora_de_areas/Vista/LogicaNegocio/CalculadoraDeAreas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public static class CalculadoraDeAreas
+    {
+        private static double area;
+
+        public static double CalcularAreaCuadrado(double longitudLado) {
+
+            area = Math.Pow(longitudLado, 2);
+
+            return area;
+        }
+
+        public static double CalcularAreaTriangulo(double basse, double altura) {
+
+            area = (basse * altura) / 2;
+
+            return area;
+        }
+
+        public static double CalcularAreaCirculo(double radio) {
+
+            area = Math.PI * Math.Pow(radio, 2);    
+
+            return area;
+        }
+
+    }
+}
---
+++ Clase_02/I06_Calculadora_de_areas/Vista/Vista/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using LogicaNegocio;
+
+namespace Vista
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta;
+            double resultado;
+            double basse;
+            double altura;
+            double radio;
+            bool validacion;
+
+            do
+            {
+                Console.WriteLine("Seleccione el area de que figura desea encontrar: \n 1_Area de un triangulo \n 2_Area de un cuadrado \n 3_Area de un circulo");
+                respuesta = Console.ReadLine();
+
+            } while (respuesta != "1" && respuesta != "2" && respuesta != "3");
+
+
+            switch (respuesta)
+            {
+                case "1":
+                    do
+                    {
+                        Console.WriteLine("Ingrese la base del triangulo");
+                        validacion = double.TryParse(Console.ReadLine(), out basse);
+                    } while (!validacion);
+
+                    do
+                    {
+                        Console.WriteLine("Ingrese la altura del triangulo");
+                        validacion = double.TryParse(Console.ReadLine(), out altura);
+                    } while (!validacion);
+
+                    resultado = CalculadoraDeAreas.CalcularAreaTriangulo(basse, altura);
+                    Console.WriteLine($"El area del triangulo es {resultado}");
+                    break;
+                case "2":
+                    do
+                    {
+                        Console.WriteLine("Ingrese la altura del cuadrado");
+                        validacion = double.TryParse(Console.ReadLine(), out altura);
+
+                    } while (!validacion);
+
+                    resultado = CalculadoraDeAreas.CalcularAreaCuadrado(altura);
+                    Console.WriteLine($"El area del cuadrado es {resultado}");
+
+                    break;
+                case "3":
+                    do
+                    {
+                        Console.WriteLine("Ingrese el radio del circulo");
+                        validacion = double.TryParse(Console.ReadLine(), out radio);
+
+                    } while (!validacion);
+
+                    resultado = CalculadoraDeAreas.CalcularAreaCirculo(radio);
+                    Console.WriteLine($"El area del circulo es {resultado}");
+
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
